798b39b58f8164cf8c3cdfe8c44bf0e7e31d088b
Heli Devine <heli.devine@gmail.com>
2021-10-28 14:51:18 +0100

    added growup function to virtual pet

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,6 +2,24 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+      expect(new Pet('Richard')).toBeInstanceOf(Object);
+    });
+    it('sets the name property', () => {
+      const pet = new Pet('Richard');
+
+      expect(pet.name).toBe('Richard');
+    });
+    it('has a initial age of 0', () => {
+      const pet = new Pet('Richard');
+  
+      expect(pet.age).toEqual(0);
+    });
+    it('increments the age by 1', () => {
+      const pet = new Pet('Richard');
+  
+      pet.growUp();
+  
+      expect(pet.age).toEqual(1);
     });
   });
+
--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,16 @@
-function Pet() {}
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+    this.growUp = function() {
+        this.age += 1;
+      };
+}
+const pet = {};
+
+pet.name = 'Richard';
 
 module.exports = Pet;
+
+
+
+
