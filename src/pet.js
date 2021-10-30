7cb667a91634d344a8fcd8251eb0e32afdbcd431
Heli Devine <heli.devine@gmail.com>
2021-10-30 14:25:22 +0100

    refactored code by setting MAX fitness ad MIN hunger

--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,10 @@
 const MAXIMUM_FITNESS = 10;
+const MINIMUM_HUNGER = 0;
 function Pet(name) {
     this.name = name;
     this.age = 0;
-    this.hunger = 0;
-    this.fitness = 10;
+    this.hunger = MINIMUM_HUNGER;
+    this.fitness = MAXIMUM_FITNESS;
     this.growUp = function() {
         this.age += 1;
         this.hunger +=5;
@@ -11,17 +12,17 @@ function Pet(name) {
       };
     
     this.walk = function(){
-      if ((this.fitness + 4) <= 10 ) {
+      if ((this.fitness + 4) <= MAXIMUM_FITNESS ) {
     this.fitness += 4;
   } else {
-    this.fitness = 10;
+    this.fitness = MAXIMUM_FITNESS;
   }
     }
     this.feed = function() {
-      if ((this.hunger - 3) >= 0 ){
+      if ((this.hunger - 3) >= MINIMUM_HUNGER ){
         this.hunger -= 3;
       } else { 
-        this.hunger = 0;
+        this.hunger = MINIMUM_HUNGER;
       }
 
       }
